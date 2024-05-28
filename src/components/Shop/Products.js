d65a4825b6552c14c3bede9ad3ff2692ad1cc999
georgezikos <53711433+georgezikos@users.noreply.github.com>
2024-05-28 15:16:23 -0400

    Added dot navigation to slider; need to re-integrate opacity transitions next

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -86,17 +86,93 @@ async function showVariantImages(variantId) {
   }
 }
 
+function navigation(slider) {
+  let wrapper, dots;
+
+  function markup(remove) {
+    wrapperMarkup(remove);
+    dotMarkup(remove);
+  }
+
+  function removeElement(element) {
+    element.parentNode.removeChild(element);
+  }
+
+  function createDiv(className) {
+    var div = document.createElement('div');
+    var classNames = className.split(' ');
+    classNames.forEach((name) => div.classList.add(name));
+    return div;
+  }
+
+  function wrapperMarkup(remove) {
+    if (remove) {
+      var parent = wrapper.parentNode;
+      while (wrapper.firstChild)
+        parent.insertBefore(wrapper.firstChild, wrapper);
+      removeElement(wrapper);
+      return;
+    }
+    wrapper = createDiv('product-page__slider-nav-container');
+    slider.container.parentNode.appendChild(wrapper);
+    wrapper.appendChild(slider.container);
+  }
+
+  function dotMarkup(remove) {
+    if (remove) {
+      removeElement(dots);
+      return;
+    }
+    dots = createDiv('dots');
+    slider.track.details.slides.forEach((_e, idx) => {
+      var dot = createDiv('dot');
+      dot.addEventListener('click', () => slider.moveToIdx(idx));
+      dots.appendChild(dot);
+    });
+    wrapper.appendChild(dots);
+  }
+
+  function updateClasses() {
+    var slide = slider.track.details.rel;
+    Array.from(dots.children).forEach(function (dot, idx) {
+      idx === slide
+        ? dot.classList.add('dot--active')
+        : dot.classList.remove('dot--active');
+    });
+  }
+
+  slider.on('created', () => {
+    markup();
+    updateClasses();
+  });
+  slider.on('optionsChanged', () => {
+    markup(true);
+    markup();
+    updateClasses();
+  });
+  slider.on('slideChanged', () => {
+    updateClasses();
+  });
+  slider.on('destroyed', () => {
+    markup(true);
+  });
+}
+
 const initializeSlider = () => {
   if (window.slider) {
     window.slider.destroy();
   }
   const sliderElement = document.getElementById('keen-slider');
   if (sliderElement && sliderElement.children.length > 0) {
-    window.slider = new KeenSlider('#keen-slider', {
-      loop: true,
-      slidesPerView: 1,
-      spacing: 10,
-    });
+    window.slider = new KeenSlider(
+      '#keen-slider',
+      {
+        loop: true,
+        slidesPerView: 1,
+        spacing: 10,
+      },
+      [navigation]
+    ); // Include the navigation plugin here
   } else {
     console.error('No slides to initialize the slider with.');
   }
