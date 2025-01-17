7c21f022b8145eb3de993cdcfbd190c9cc45fe6a
George Zikos <george.zikos@icloud.com>
2025-01-17 16:30:33 -0500

    Small tweak

--- src/components/Utility/CopyrightYear.js
+++ src/components/Utility/CopyrightYear.js
@@ -1,7 +1,10 @@
 const CopyrightYear = () => {
   const currentYear = new Date().getFullYear();
-  document.getElementById('copyright-year-mobile').textContent = currentYear;
-  document.getElementById('copyright-year-desktop').textContent = currentYear;
+  const mobileElement = document.getElementById('copyright-year-mobile');
+  const desktopElement = document.getElementById('copyright-year-desktop');
+
+  if (mobileElement) mobileElement.textContent = currentYear;
+  if (desktopElement) desktopElement.textContent = currentYear;
 };
 
 export default CopyrightYear;
