1aa4f9574bad8183cbc86ebe93541bc1ce114389
George Zikos <george.zikos@icloud.com>
2025-08-26 16:26:38 -0400

    Setting up Tailwind

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,36 @@ const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/global.js',
-  output: {
-    filename: 'global.js',
-    path: path.resolve(__dirname, 'assets'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
+    mode: 'development',
+    entry: './src/global.js',
+    output: {
+        filename: 'global.js',
+        path: path.resolve(__dirname, 'assets'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader', 'postcss-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                { from: './src/global.css', to: 'global.css' },
+                { from: './src/v2-1-0.css', to: 'v2-1-0.css' },
+            ],
+        }),
     ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [{ from: './src/global.css', to: 'global.css' }],
-    }),
-  ],
 };
